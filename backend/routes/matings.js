37fe4ce010de7a48c361e32f28d011a3a61fa2ea
zencodie <zencodie@gmail.com>
2026-05-14 00:21:24 +0530

    Fix incorrect middleware import path

--- backend/routes/matings.js
+++ backend/routes/matings.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const matingController = require('../controllers/matingController');
-const { protect } = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/auth');
 
 router.use(protect);
 
